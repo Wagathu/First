a52f534fbe75e0c794949d9953784932f28de385
stanleyrazor <stanleysayankaSSS@gmail.com>
2022-12-27 20:46:31 +0300

    Update to include odd and eve functions
    This is a major update to include odd and even functions

--- checkNumber.R
+++ checkNumber.R
@@ -1,8 +1,15 @@
 
-checkNumber <- function(n)
+checkNumber_even <- function(n)
 {
   msg = ifelse(n %% 2 == 0, "Even Number", "Not even")
   return(msg)
 
 }
-checkNumber(2)
+checkNumber_even(2)
+
+checkNumber_odd <- function(n)
+{
+  msg = ifelse(n %% 2 != 0, "Odd Number", "Not Odd")
+  return(msg)
+}
+checkNumber_odd(7)
